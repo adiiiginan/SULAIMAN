e74aa907bf16dd1d48176d6ca5cfaa18f2705100
adiginan <adiginandani28@gmail.com>
2025-05-20 13:26:20 +0700

    sulaiman baru

---
+++ resources/views/Login/login.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <base href="../../../" />
+    <title>RSAU LANUD SULAIMAN</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
+    <link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+</head>
+<body id="kt_body" class="app-blank bgi-size-cover bgi-attachment-fixed bgi-position-center bgi-no-repeat">
+    <script>
+        // Atur tema
+        var defaultThemeMode = "light";
+        var themeMode;
+        if (document.documentElement) {
+            if (document.documentElement.hasAttribute("data-bs-theme-mode")) {
+                themeMode = document.documentElement.getAttribute("data-bs-theme-mode");
+            } else {
+                themeMode = localStorage.getItem("data-bs-theme") ?? defaultThemeMode;
+            }
+            if (themeMode === "system") {
+                themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+            }
+            document.documentElement.setAttribute("data-bs-theme", themeMode);
+        }
+    </script>
+
+    <!-- Halaman Login -->
+    <div class="d-flex flex-column flex-root" id="kt_app_root">
+        <style>
+            body {
+                background-image: url('assets/media/auth/bg4.jpg');
+            }
+            [data-bs-theme="dark"] body {
+                background-image: url('assets/media/auth/bg4-dark.jpg');
+            }
+        </style>
+
+        <div class="d-flex flex-column flex-column-fluid flex-lg-row">
+            <!-- Aside -->
+            <div class="d-flex flex-center w-lg-50 pt-15 pt-lg-0 px-10">
+                <div class="d-flex flex-center flex-lg-start flex-column">
+                    <a href="#" class="mb-7">
+                        <img alt="Logo" src="assets/media/logos/custom-3.svg" />
+                    </a>
+                </div>
+            </div>
+
+            <!-- Form Login -->
+            <div class="d-flex flex-column-fluid flex-lg-row-auto justify-content-center justify-content-lg-end p-12 p-lg-20">
+                <div class="bg-body d-flex flex-column align-items-stretch flex-center rounded-4 w-md-600px p-20">
+                    <div class="d-flex flex-center flex-column flex-column-fluid px-lg-10 pb-15 pb-lg-20">
+                        <!-- Form -->
+                        <form class="form w-100" id="kt_sign_in_form" action="/ranap" method="GET">
+                            <div class="text-center mb-11">
+                                <h1 class="text-gray-900 fw-bolder mb-3">Sign In</h1>
+                                <div class="text-gray-500 fw-semibold fs-6">ELECTRONIC REKAM MEDICAL</div>
+                            </div>
+
+                            <div class="fv-row mb-8">
+                                <input type="text" placeholder="Email" name="email" autocomplete="off" class="form-control bg-transparent" />
+                            </div>
+
+                            <div class="fv-row mb-3">
+                                <input type="password" placeholder="Password" name="password" autocomplete="off" class="form-control bg-transparent" />
+                            </div>
+
+                            <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
+                                <div></div>
+                                <a href="#" class="link-primary">Forgot Password ?</a>
+                            </div>
+
+                            <div class="d-grid mb-10">
+                                <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
+                                    <span class="indicator-label">Sign In</span>
+                                    <span class="indicator-progress">Please wait...
+                                        <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+                                    </span>
+                                </button>
+                            </div>
+                        </form>
+                        <!-- End Form -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Script -->
+    <script src="assets/plugins/global/plugins.bundle.js"></script>
+    <script src="assets/js/scripts.bundle.js"></script>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('login/login');
+});
+
+Route::get('/', function () {
+    return view('ranap/index');
+});
